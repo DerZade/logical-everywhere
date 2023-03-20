30d9b17f3ae266de9fedb2b454e98ae152b0a2b0
DerZade <DerZade@gmail.com>
2023-03-20 14:39:07 +0100

    chore: :tada: initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# Logical Properties EVERYWHERE
+
+_Collection of utilities to work around features that are not compatible with logical properties (such as [`translateX`](https://developer.mozilla.org/docs/Web/CSS/transform-function/translateX), [`translateY`](https://developer.mozilla.org/docs/Web/CSS/transform-function/translateY) or [`getBoundingClientRect`](https://developer.mozilla.org/docs/Web/API/Element/getBoundingClientRect))._
+
+## Motivation
+
+When positioning elements with JS, it is difficult to work with logical properties in mind. For example, if I want to display a tooltip in the `block-start `direction of an element (usually physically above), I first need to know where the inline and block axes are located in order to then position the tooltip in the correct physical direction (`top`, `left`, `bottom` or right):
+
+This package aims to simplifies this process.
+
+## Installation
+
+```
+npm i --save logical-properties-everywhere
+```
+
+## Utilities
+
+### `getPhysicalDirection`
+
+Get the physical direction that corresponds to the logical direction of a element.
+
+```ts
+const myElement = document.getElementById('my-element');
+
+const physicalDirection = getPhysicalDirection(myElement, 'inline-start');
+
+console.log(physicalDirection); // prints "left"
+```
+
+### `getPhysicalFlexDirection`
+
+Get physical axis of the `flex-direction` CSS property.
+
+```ts
+const myElement = document.getElementById('my-element');
+
+myElement.style.flexDirection = 'row-reverse';
+const axisRR = getPhysicalFlexDirection(myElement);
+console.log(axisRR); // prints "right-left"
+
+myElement.style.flexDirection = 'column-reverse';
+const axisCR = getPhysicalFlexDirection(myElement);
+console.log(axisCR); // prints "bottom-top"
+```
+
+### `getElementAxes`
+
+Get inline- and block-axis of a HTML element.
+
+```ts
+const { block, inline } = getElementAxes(myElement);
+
+console.log(inline); // prints "left-right"
+console.log(block); // prints "top-bottom"
+```
+
+### `axisDimensionAndDirection`
+
+Get dimension and direction of a physical axis.
+
+This is especially useful when trying to calculate a dynamic `translateX` / `translateY`.
+
+```ts
+const myElement = document.getElementById('my-element');
+
+const { inline } = getElementAxes(myElement);
+
+const { dimension, multiplier } = axisDimensionAndDirection(inline);
+
+const transform = `translate${dimension.toUpperCase()}(${3 * multiplier})`;
+```
+
+### `axisToStartEnd`
+
+Get from- and to-direction of a physical axis.
+
+```ts
+const { start, end } = axisToStartEnd('top-bottom');
+console.log(start); // prints "top"
+console.log(end); // prints "bottom"
+```
+
+### `reverseAxis`
+
+Reverse a physical axis
+
+```ts
+const reversed = reverseAxis('top-bottom');
+
+console.log(reversed); // prints "bottom-top"
+```
---
+++ src/index.ts
@@ -0,0 +1,206 @@
+/** A logical direction */
+export type LogicalDirection = `${'inline' | 'block'}-${'start' | 'end'}`;
+
+/** A physical direction */
+export type PhysicalDirection = 'top' | 'bottom' | 'right' | 'left';
+
+/**
+ * A physical axis. (`{from}-{to}`)
+ */
+export type PhysicalAxis =
+    | 'left-right'
+    | 'top-bottom'
+    | 'right-left'
+    | 'bottom-top';
+
+/**
+ * An object containing the physical axes corresponding to the logical axes
+ */
+export interface Axes {
+    block: PhysicalAxis;
+    inline: PhysicalAxis;
+}
+
+/** CSS `writing-mode` property */
+type CSSWritingMode =
+    | 'horizontal-tb'
+    | 'vertical-rl'
+    | 'vertical-lr'
+    | 'sideways-rl'
+    | 'sideways-lr';
+
+/**
+ * Convert a string to concrete union of possible writing modes.
+ */
+export function toWritingMode(val: string): CSSWritingMode {
+    const writingMode = (
+        {
+            'horizontal-tb': 'horizontal-tb',
+            'vertical-rl': 'vertical-rl',
+            'vertical-lr': 'vertical-lr',
+            'sideways-rl': 'sideways-rl',
+            'sideways-lr': 'sideways-lr',
+            lr: 'horizontal-tb',
+            'lr-tb': 'horizontal-tb',
+            rl: 'horizontal-tb',
+            tb: 'vertical-lr',
+            'tb-lr': 'vertical-lr',
+            'tb-rl': 'vertical-rl'
+        } as const
+    )[val];
+
+    return writingMode ?? 'horizontal-tb';
+}
+
+/** CSS `direction` property */
+type CSSDirection = 'ltr' | 'rtl';
+
+/**
+ * Convert a string to concrete union of possible directions.
+ */
+export function toDirection(val: string): CSSDirection {
+    return val === 'rtl' ? 'rtl' : 'ltr';
+}
+
+/** Object mapping WritingMode and Direction to axes */
+export const AXES_MAP = {
+    'horizontal-tb': {
+        ltr: { inline: 'left-right', block: 'top-bottom' },
+        rtl: { inline: 'right-left', block: 'top-bottom' }
+    },
+    'vertical-rl': {
+        ltr: { inline: 'top-bottom', block: 'right-left' },
+        rtl: { inline: 'bottom-top', block: 'right-left' }
+    },
+    'vertical-lr': {
+        ltr: { inline: 'top-bottom', block: 'left-right' },
+        rtl: { inline: 'bottom-top', block: 'left-right' }
+    },
+    'sideways-rl': {
+        ltr: { inline: 'top-bottom', block: 'right-left' },
+        rtl: { inline: 'bottom-top', block: 'right-left' }
+    },
+    'sideways-lr': {
+        ltr: { inline: 'bottom-top', block: 'left-right' },
+        rtl: { inline: 'top-bottom', block: 'left-right' }
+    }
+} satisfies { [wm in CSSWritingMode]: { [dir in CSSDirection]: Axes } };
+
+/**
+ * Get inline- and block-axis of a HTML element.
+ * @param el HTML Element
+ * @returns An object, containing `inline`- and `block`-axis
+ */
+export function getElementAxes(el: HTMLElement): Axes {
+    const { writingMode: writingModeStr, direction: directionStr } =
+        getComputedStyle(el);
+
+    const writingMode = toWritingMode(writingModeStr);
+    const direction = toDirection(directionStr);
+
+    return AXES_MAP[writingMode][direction];
+}
+
+/**
+ * Get dimension and direction of a physical axis.
+ * @param axis Axis
+ * @returns An object, containing the dimension (`"x"` or `"y"`) and a multiplier to adjust for the direction.
+ */
+export function axisDimensionAndDirection(axis: PhysicalAxis): {
+    dimension: 'x' | 'y';
+    multiplier: -1 | 1;
+} {
+    return (
+        {
+            'left-right': { dimension: 'x', multiplier: 1 },
+            'top-bottom': { dimension: 'y', multiplier: 1 },
+            'right-left': { dimension: 'x', multiplier: -1 },
+            'bottom-top': { dimension: 'y', multiplier: -1 }
+        } as const
+    )[axis];
+}
+
+/**
+ * Get from- and to-direction of a physical axis
+ * @param axis Axis
+ * @returns An object, containing the `start`- and `end`-direction of the axis.
+ */
+export function axisToStartEnd(axis: PhysicalAxis): {
+    start: PhysicalDirection;
+    end: PhysicalDirection;
+} {
+    return (
+        {
+            'bottom-top': { start: 'bottom', end: 'top' },
+            'top-bottom': { start: 'top', end: 'bottom' },
+            'left-right': { start: 'left', end: 'right' },
+            'right-left': { start: 'right', end: 'left' }
+        } as const
+    )[axis];
+}
+
+/**
+ * Reverse a physical axis
+ * @param axis Axis to reverse
+ * @returns Reversed axis
+ */
+export function reverseAxis(axis: PhysicalAxis): PhysicalAxis {
+    return (
+        {
+            'bottom-top': 'top-bottom',
+            'top-bottom': 'bottom-top',
+            'left-right': 'right-left',
+            'right-left': 'left-right'
+        } as const
+    )[axis];
+}
+
+/**
+ * Get the physical direction that corresponds to the logical direction of a element.
+ *
+ * @param el Element to get directions for
+ * @param logicalDirection Logical direction
+ * @returns Physical direction representing the logical direction
+ */
+export function getPhysicalDirection(
+    el: HTMLElement,
+    logicalDirection: LogicalDirection
+): PhysicalDirection {
+    const { inline, block } = getElementAxes(el);
+
+    switch (logicalDirection) {
+        case 'inline-start':
+            return axisToStartEnd(inline).start;
+
+        case 'inline-end':
+            return axisToStartEnd(inline).end;
+
+        case 'block-start':
+            return axisToStartEnd(block).start;
+
+        case 'block-end':
+            return axisToStartEnd(block).end;
+    }
+}
+
+/**
+ * Get physical axis of the `flex-direction` CSS property.
+ *
+ * @param el HTML Element
+ */
+export function getPhysicalFlexDirection(el: HTMLElement): PhysicalAxis {
+    const { inline, block } = getElementAxes(el);
+
+    const { flexDirection } = getComputedStyle(el);
+
+    switch (flexDirection) {
+        case 'row-reverse':
+            return reverseAxis(inline);
+        case 'column':
+            return block;
+        case 'column-reverse':
+            return reverseAxis(block);
+        default:
+            return inline;
+    }
+}
