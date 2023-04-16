54385cd889f228857dee8f9901848728624deafa
DerZade <DerZade@gmail.com>
2023-04-16 16:35:47 +0200

    feat: :sparkles: add CSSFlexWrap type and toFlexWrap util

---
+++ test/toFlexWrap.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest';
+import { toFlexWrap } from '../src';
+
+describe('toFlexWrap', () => {
+    it('should return nowrap for invalid values', () => {
+        expect(toFlexWrap('blub')).toBe('nowrap');
+    });
+    it('should accept "nowrap"', () => {
+        expect(toFlexWrap('nowrap')).toBe('nowrap');
+    });
+    it('should accept "wrap"', () => {
+        expect(toFlexWrap('wrap')).toBe('wrap');
+    });
+    it('should accept "wrap-reverse"', () => {
+        expect(toFlexWrap('wrap-reverse')).toBe('wrap-reverse');
+    });
+});
